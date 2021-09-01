db2f1f1f445636aa15a804856396d60416650977
hsiaofongw <hsiaofong.w@gmail.com>
2021-09-02 02:44:27 +0800

    build: 优化了编译打包命令

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
 
-process.on("message", (markdownText: string) => {
+process.on('message', (markdownText: string) => {
   const tree = unified().use(remarkParse).parse(markdownText);
   if (!!process.send) {
     process.send(JSON.stringify(tree));
